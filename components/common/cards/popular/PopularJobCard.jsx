81338f086a427bc4b3b8fc7aba713f57bf2af51b
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2023-11-16 18:58:51 +0100

    update

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -17,7 +17,7 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
         />
       </TouchableOpacity>
       <Text style={styles.companyName} numberOfLines={1}>
-        {item.employer_name}{" "}
+        {item.employer_name}
       </Text>
       <View style={styles.infoContainer}>
         <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>{item.job_title}</Text>
